ece19be6a09fb740147d8b8a79160a6548b2ddeb
Mads Plenge <madskp@fend01.cluster>
2024-10-08 08:49:20 +0200

    full grid search

--- stability_space/grid_sim/combine_subgrids.py
+++ stability_space/grid_sim/combine_subgrids.py
@@ -4,7 +4,7 @@ import numpy as np
 
 subgrids = [np.loadtxt(f'grid_sim_{str(i).zfill(2)}.csv',delimiter=',') for i in range(100)]
 
-grids_comb = np.vstack(grids)
+grids_comb = np.vstack(subgrids)
 
 np.savetxt('grids_full.csv',grids_comb,delimiter=',',fmt='%1.6f')
 
